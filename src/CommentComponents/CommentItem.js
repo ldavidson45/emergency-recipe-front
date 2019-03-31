c67dc2ea0665574bd9459b47de31efe1f77aaff6
Liz Davidson <lizdavidson45@gmail.com>
2019-03-31 12:36:39 -0400

    add style to comment items

--- src/CommentComponents/CommentItem.js
+++ src/CommentComponents/CommentItem.js
@@ -1,10 +1,15 @@
 import React from "react";
+import "./Comment.css";
+import { Icon, Button } from "react-materialize";
 
 function CommentItem(comment) {
   return (
     <div>
-      <p>
-        <span className="comment-author">{comment.name}</span> -{" "}
+      <p className="comment-item-container">
+        <Icon className="user-icon" tiny>
+          perm_identity
+        </Icon>
+        <span className="comment-author">{comment.name}</span>
         {comment.content}
       </p>
     </div>
