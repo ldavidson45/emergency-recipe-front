56721a4c75f294932b590d6f1095686e4279548c
Liz Davidson <lizdavidson45@gmail.com>
2019-04-01 21:20:17 -0400

    tweak navbar style

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import { Icon } from "react-materialize";
 
 class NavBar extends Component {
   render() {
     return (
       <div className="nav-bar">
+        <Link to="/" className="home-icon">
+          <Icon>home</Icon>
+        </Link>
         <Link className="nav-link" to="/login">
           <span>Log In / Sign Up</span>
         </Link>
